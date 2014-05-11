d09a5d4d3fb1bf746351992b9aaa8a74ccef8b37
tnyadav <tny0485@gmail.com>
2014-05-11 22:02:34 +0530

    treeview update

--- Ordrit/src/com/ordrit/fragment/MenuFragment.java
+++ Ordrit/src/com/ordrit/fragment/MenuFragment.java
@@ -1,21 +1,22 @@
 package com.ordrit.fragment;
 
-import com.ordrit.R;
-import com.ordrit.util.FragmentConstant;
+import java.util.HashSet;
+import java.util.Set;
 
-import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+
+import com.ordrit.R;
+import com.ordrit.adapter.TreeListAdapter;
+import com.ordrit.treeview.InMemoryTreeStateManager;
+import com.ordrit.treeview.TreeBuilder;
+import com.ordrit.treeview.TreeStateManager;
+import com.ordrit.treeview.TreeViewList;
+import com.ordrit.util.FragmentConstant;
 
 public class MenuFragment extends BaseFragment {
 	
@@ -23,6 +24,17 @@ public class MenuFragment extends BaseFragment {
 	private Button menuAccount, menuBag, menuOrderStatus,
 			menuPreviousOrder, menuCoupon, menuHelp, menuShoping;
 
+
+    private final Set<Long> selected = new HashSet<Long>();
+
+    private TreeViewList treeView;
+
+    private static final int[] DEMO_NODES = new int[] { 0, 0,0, 1, 1, 2, 2, 1,
+            1, 2, 1, 0, 0, 0/*, 1, 2, 3, 2, 0, 0, 1, 2, 0, 1, 2, 0, 1*/ };
+    private static final int LEVEL_NUMBER = 4;
+    private TreeStateManager<Long> manager = null;
+    private TreeListAdapter simpleStandardAdapter;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -98,6 +110,19 @@ public class MenuFragment extends BaseFragment {
 				
 			}
 		});
+		 manager = new InMemoryTreeStateManager<Long>();
+         final TreeBuilder<Long> treeBuilder = new TreeBuilder<Long>(manager);
+         for (int i = 0; i < DEMO_NODES.length; i++) {
+             treeBuilder.sequentiallyAddNextNode((long) i, DEMO_NODES[i]);
+         }
+         treeView = (TreeViewList) menuFragment.findViewById(R.id.mainTreeView);
+       
+         simpleStandardAdapter = new TreeListAdapter(dashboardActivity, selected, manager,
+                 LEVEL_NUMBER);
+         treeView.setAdapter(simpleStandardAdapter);
+         treeView.setCollapsible(true);
+         manager.collapseChildren(null);
+       //  treeView.setCollapsible(false);
 	}
 
 }

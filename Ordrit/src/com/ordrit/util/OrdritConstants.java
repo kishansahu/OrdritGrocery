175c4a8657ce7bcb662b846e10b755151af3c0af
unknown <trilokinathyadav@HPDA0050.qait.com>
2014-05-21 17:57:15 +0530

    update

--- Ordrit/src/com/ordrit/util/OrdritConstants.java
+++ Ordrit/src/com/ordrit/util/OrdritConstants.java
@@ -4,10 +4,12 @@ public interface OrdritConstants {
 
 	public final static String SERVER_BASE_URL = "http://staging.ankursethi.in/";
 	public final static String API_TOKEN_URL = SERVER_BASE_URL+"api-token/";
+	public final static String USERS = "users";
+	public final static String USERS_ADDRESS ="user_addresses";
 	//doc
-	//public final static String SUPER_USER_TOKEN_ID = "32d1f38daad2c61656addd78f7d47afaadcc6bc0";
+	public final static String SUPER_USER_TOKEN_ID = "f8217f2bfca176c7c2ef6c45a7c582d17668a0ec";
 	//current
-	public final static String SUPER_USER_TOKEN_ID = "b137f7b6394b1b9fbac6233a12c5937e511fa3dd";
+	//public final static String SUPER_USER_TOKEN_ID = "b137f7b6394b1b9fbac6233a12c5937e511fa3dd";
 	
 	
 }
